d8e67fbc4bcb72087eb05b574e6f1c7634481daf
AnhDV <bkavprovn.net>
2017-06-30 19:23:33 +0700

    KAROFI

--- news.php
+++ news.php
@@ -4,12 +4,32 @@
 	<title>news</title>
 </head>
 <?php
+<<<<<<< HEAD
 	$connect = mysqli_connect('', '', '');
 	$query = mysql_query('SQL');
 	$data = mysqli_fetch_assoc($query);
 
 
+=======
+	
+>>>>>>> KAROFI
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	echo "sung đã viết dòng này";
+	echo "KAROFI";
 ?>
 <body>
 <h1>news</h1>
